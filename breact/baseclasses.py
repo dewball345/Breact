90d7fefc0a6582e5acc77457e5a63371dd8468fd
dewball345 <abhiraamkumar@gmail.com>
2021-05-08 16:52:34 -0700

    Added dom diffing

--- breact/baseclasses.py
+++ breact/baseclasses.py
@@ -12,7 +12,47 @@ class GenerateContainers:
         self.container = html.DIV(id=self.outerId)
         self.content = html.DIV(id=self.innerId)
 
+
 class StatefulSegment(Base):
+    def __init__(self):
+        self.oi = GenerateContainers()
+        self.state = {}
+    def render(self):
+        self.oi.content <= self.update()
+        self.oi.container <= self.oi.content
+        return self.oi.container
+    def update(self):
+        pass
+    def setState(self, attrs, one_state_change=False):
+        self.state.update(attrs) 
+        def find_diffs(o, n, op):
+            oldChildren = o
+            newChildren = n
+            for i in range(min(len(oldChildren), len(newChildren))):
+                if(str(oldChildren[i]) == str(newChildren[i])):
+                    if(len(oldChildren[i].children) == 0 and len(newChildren[i].children) == 0):
+                        if(oldChildren[i].innerHTML != newChildren[i].innerHTML):
+                            oldChildren[i].innerHTML = newChildren[i].innerHTML
+                    else:
+                        find_diffs(oldChildren[i].children, newChildren[i].children, oldChildren[i])
+                elif(str(oldChildren[i]) != str(newChildren[i])):
+                    print(str(oldChildren[i]), "not_equal_to", str(newChildren[i]))
+                    oldChildren[i].parentNode.replaceChild(newChildren[i], oldChildren[i])
+
+            if len(newChildren) > len(oldChildren):
+                for i in newChildren[len(oldChildren):]:
+                    print(str(i))
+                    op <= i
+            elif len(oldChildren) > len(newChildren):
+                for i in oldChildren[len(newChildren):]:
+                    i.remove()
+        print("SETSTATE")
+        old = document[self.oi.innerId].children
+        update = self.update(one_state_change)
+        uChildren = [update]
+        find_diffs(old, uChildren, document[self.oi.innerId])
+
+class OLD_StatefulSegment(Base):
     def __init__(self):
         self.oi = GenerateContainers()
         self.state = {}
